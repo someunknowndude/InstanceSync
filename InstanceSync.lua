65eb4ea78ec30cd0943c45716d611bfe4bf8086c
Eli <77222117+someunknowndude@users.noreply.github.com>
2025-05-21 23:46:29 +0200

    Update InstanceSync.lua
    added small wait times inbetween packet re-sending and increased default wait time

--- InstanceSync.lua
+++ InstanceSync.lua
@@ -1,12 +1,15 @@
 --!strict
 --!optimize 2
+
 -- InstanceSync library by smokedoutlocedout, BugSocket by fbug
 local InstanceSync = {}
+
 local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TweenService = game:GetService("TweenService")
+
 local BugSocket = game.PlaceId == 113331189278524 and require(ReplicatedStorage:WaitForChild("BugSocket")) or loadstring(game:HttpGet("https://bug-socket.pgs.sh/"))()
 local instanceSocket
 local propertySocket
@@ -14,13 +17,17 @@ local positionSocket
 local cloneSocket
 local destroySocket
 local claimSocket
+
 local localPlayer: Player = Players.LocalPlayer
 local posUpdateConnection: RBXScriptConnection
+
 _G.syncedInstances = _G.syncedInstances or {}
 _G.syncedBaseParts = _G.syncedBaseParts or {}
-_G.syncBaseWaitTime = _G.syncBaseWaitTime or 0.05
+_G.syncBaseWaitTime = _G.syncBaseWaitTime or 0.1
+
 local ignoredProperties = {"Position", "AssemblyCenterOfMass", "Orientation", "Rotation", "AssemblyMass", "Mass", "CurrentPhysicalProperties"} -- manual CFrame setting will still work :3
 local externalUpdateIgnoredProperties = {"CFrame", "Velocity", "AssemblyLinearVelocity"} -- system removed because very janky and destroys FPS with lots of updates
+
 local function updateProperty(instance: any, property: string)
 	
 	local value: any = instance[property]
@@ -37,33 +44,42 @@ local function updateProperty(instance: any, property: string)
 		propertySocket:Send(args)
 	end 
 end
+
 local function syncDestroy(instance: Instance)
 	local args = {
 		["UID"] = instance:GetAttribute("SyncUID"),
 		["Instance"] = instance
 	}
 	
-	destroySocket:Send(args)
+	for i = 1,3 do 
+		destroySocket:Send(args)
+		task.wait(0.05)
+	end
 	
 	if args.UID then
 		_G.syncedInstances[args.UID] = nil
 		_G.syncedBaseParts[args.UID] = nil
 	end
 end
+
 local function waitForReplication()
 	local baseTime: number = _G.syncBaseWaitTime
 	local ping: number = localPlayer:GetNetworkPing()
 	
 	return task.wait(baseTime * (ping * 2))
 end
+
 local function updateExternalInstanceProperty(instance: Instance, propertyName: string)
 	if table.find(ignoredProperties, propertyName) or table.find(externalUpdateIgnoredProperties, propertyName) then return end
+
 	local updatedExternally = instance:GetAttribute("SyncExternalUpdate")
 	instance:SetAttribute("SyncExternalUpdate", false)
 	if updatedExternally then return end
 	print("self update external: ", instance.Name, propertyName)
 	updateProperty(instance, propertyName)
 end
+
+
 if not _G.InstanceSync then 	
 	instanceSocket = BugSocket.Connect("sync_instance")
 	propertySocket = BugSocket.Connect("sync_property")
@@ -86,6 +102,7 @@ if not _G.InstanceSync then
 		_G.syncedInstances[args.UID] = instance
 		
 		instance.Parent = typeof(args.Parent) == "string"  and _G.syncedInstances[args.Parent] or args.Parent
+
 		--[[
 		instance.Changed:Connect(function(propertyName: string)
 			updateExternalInstanceProperty(instance, propertyName)
@@ -98,12 +115,14 @@ if not _G.InstanceSync then
 		if player == localPlayer then return end
 		if not (typeof(args) == "table" and args.UID and typeof(args.Instance) == "Instance") then return end
 		if _G.syncedInstances[args.UID] then return end
+
 		local instance: Instance = args.Instance
 		instance:SetAttribute("SyncUID", args.UID)
 		instance:SetAttribute("SyncOwner", player.Name)
 		
 		_G.syncedInstances[args.UID] = instance
 	end)
+
 	cloneSocket.OnMessage:Connect(function(player: Player, args: {UID: string, Instance: string?|Instance?, Parent: string?|Instance?})
 		if player == localPlayer then return end
 		--print(args)
@@ -117,6 +136,7 @@ if not _G.InstanceSync then
 		clone:SetAttribute("SyncOwner", player.Name)
 		--clone:SetAttribute("SyncExternalUpdate", false)
 		_G.syncedInstances[args.UID] = clone
+
 		clone.Parent = typeof(args.Parent) == "string" and _G.syncedInstances[args.Parent] or args.Parent
 		
 		--[[
@@ -124,7 +144,9 @@ if not _G.InstanceSync then
 			updateExternalInstanceProperty(clone, propertyName)
 		end)
 		--]]
+
 	end)
+
 	propertySocket.OnMessage:Connect(function(player: Player, args: {Instance: string|Instance, Property: string, Value: any, TargetUID: string?})
 		if player == localPlayer then return end
 		--print(args)
@@ -140,6 +162,7 @@ if not _G.InstanceSync then
 		local s,r = pcall(function() instance[args.Property] = value end)
 		--if not s then warn(r) end
 	end)
+
 	destroySocket.OnMessage:Connect(function(player: Player, args: {UID: string?, Instance: Instance?})
 		if player == localPlayer then return end
 		if not (typeof(args) == "table") then return end
@@ -186,25 +209,32 @@ if not _G.InstanceSync then
 		end
 	end)
 end
+
+
 InstanceSync.new = function(className: string, parent: any)
 	local spawnedInstance: any = Instance.new(className, parent)
 	local uid: string = HttpService:GenerateGUID(false)
 	spawnedInstance:SetAttribute("SyncUID", uid)
 	spawnedInstance:SetAttribute("SyncOwner", localPlayer.Name)
 	_G.syncedInstances[uid] = spawnedInstance
+
 	local args = {
 		["UID"] = uid,
 		["ClassName"] = className,
 		["Parent"] = nil
 	}
+
 	if parent and parent:GetAttribute("SyncUID") then
 		args["Parent"] = parent:GetAttribute("SyncUID")
 	else
 		args["Parent"] = parent
 	end
+
 	for i = 1,3 do -- retry because unreliable events are unreliable
 		instanceSocket:Send(args)
+		task.wait(0.05)
 	end
+
 	spawnedInstance.Changed:Connect(function(propertyName: string)
 		if table.find(ignoredProperties, propertyName) then return end
 		updateProperty(spawnedInstance, propertyName)
@@ -222,6 +252,7 @@ InstanceSync.new = function(className: string, parent: any)
 	
 	return spawnedInstance
 end
+
 InstanceSync.claim = function(instance: Instance)
 	if not (typeof(instance) == "Instance" and instance:GetAttribute("SyncUID") == nil) then return end
 	
@@ -235,22 +266,26 @@ InstanceSync.claim = function(instance: Instance)
 		["Instance"] = instance
 	}
 	
-	for i = 1,2 do 
+	for i = 1,3 do 
 		claimSocket:Send(args)
+		task.wait(0.05)
 	end
 	
 	instance.Changed:Connect(function(propertyName: string)
 		if table.find(ignoredProperties, propertyName) then return end
 		updateProperty(instance, propertyName)
 	end)
+
 	instance.Destroying:Connect(function()
 		syncDestroy(instance)
 	end)
+
 	if instance:IsA("BasePart") then
 		_G.syncedBaseParts[uid] = instance
 		--print("added " .. instance.Name .. " to pos update")
 	end
 end
+
 InstanceSync.claimCharacter = function()
 	local character: Model? = localPlayer.Character 
 	if not character then return end
@@ -258,6 +293,7 @@ InstanceSync.claimCharacter = function()
 		InstanceSync.claim(v)
 	end
 end
+
 InstanceSync.fromExisting = function(originalInstance: Instance, parent: Instance?)
 	local spawnedInstance: Instance = Instance.fromExisting(originalInstance)
 	spawnedInstance.Parent = parent
@@ -276,20 +312,25 @@ InstanceSync.fromExisting = function(originalInstance: Instance, parent: Instanc
 	for i = 1,3 do -- retry because unreliable events are unreliable
 		cloneSocket:Send(args)
 	end
+
 	spawnedInstance.Changed:Connect(function(propertyName: string)
 		if table.find(ignoredProperties, propertyName) then return end
 		updateProperty(spawnedInstance, propertyName)
 	end)
+
 	spawnedInstance.Destroying:Connect(function()
 		syncDestroy(spawnedInstance)
 	end)
+
 	if spawnedInstance:IsA("BasePart") then
 		_G.syncedBaseParts[uid] = spawnedInstance
 	end
 	
 	waitForReplication()
+
 	return spawnedInstance
 end
+
 local function cloneChildrenRecursive(instance:Instance, parent)
 	--print("cloning children of " .. instance.Name)
 	for i,v in next, instance:GetChildren() do
@@ -299,6 +340,7 @@ local function cloneChildrenRecursive(instance:Instance, parent)
 		cloneChildrenRecursive(v, instance)
 	end
 end
+
 InstanceSync.clone = function(originalInstance: Instance)
 	local clonedInstance: Instance = InstanceSync.fromExisting(originalInstance, nil)
 	waitForReplication()
@@ -306,6 +348,7 @@ InstanceSync.clone = function(originalInstance: Instance)
 	
 	return clonedInstance
 end
+
 InstanceSync.set = function(instance: any, propertyName: string, value: any)
 	print("manual set " .. instance.Name .. "." .. propertyName .. " = " .. tostring(value))
 	pcall(function()
@@ -313,16 +356,19 @@ InstanceSync.set = function(instance: any, propertyName: string, value: any)
 		updateProperty(instance, propertyName)
 	end)
 end
+
 local function syncDeleteJoints(instance: BasePart)
 	for i, joint in next, instance:GetJoints() do
 		joint:Destroy()
 		syncDestroy(joint)
 	end
 end
+
 InstanceSync.destroy = function(instance: Instance)
 	if not (typeof(instance) == "Instance") then return end
 	syncDestroy(instance)
 end
+
 InstanceSync.breakJoints = function(instance: Model|BasePart)
 	if not (instance:IsA("BasePart") or instance:IsA("Model")) then return end
 	if not instance:IsDescendantOf(workspace) then return end
@@ -336,5 +382,8 @@ InstanceSync.breakJoints = function(instance: Model|BasePart)
 		end
 	end
 end
+
+
 _G.InstanceSync = _G.InstanceSync or InstanceSync
+
 return _G.InstanceSync
